19ce517bb7e36cfa1f4ac836b76c59f58956e448
reireias <reireias@gmail.com>
2017-11-21 11:40:09 +0900

    update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -11,20 +11,22 @@ chsh -s /usr/bin/zsh
 
 # zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-zsh -c 'zplug install'
+zsh -c 'source ~/.zshrc; zplug install'
 if [ -e ~/.zcompdump ]; then
     rm ~/.zcompdump
 fi
 if [ -e ~/.zplug/zcompdump ]; then
     rm ~/.zplug/zcompdump
 fi
-zsh -c 'compinit'
+zsh -c 'source ~/.zshrc; compinit'
 
 
 # neovim
 sudo apt-get install -y vim
 mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
 git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo apt-get update -q
 sudo apt-get install -y neovim
 ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
