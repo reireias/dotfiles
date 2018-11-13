24b493fa1f12c4d650b8d5ce3c22a1f0e67c7a8d
reireias <reireias@gmail.com>
2018-11-13 12:44:13 +0900

    fix shellcheck error

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ for dotfile in .??*; do
     if [[ -d $dotfile ]]; then
         # create directory and link file if file is directory
         find "$dotfile" -type f \
-            -exec sh -c '[ -e "$HOME/$(dirname {})" ] || mkdir -p "$HOME/$(dirname {})"' \; \
-            -exec sh -c 'ln -snfv "$(pwd)/{}" "${HOME}/{}"' \;
+            -exec sh -c '[ -e "$HOME/$(dirname "$1")" ] || mkdir -p "$HOME/$(dirname "$1")"' _ {} \; \
+            -exec sh -c 'ln -snfv "$(pwd)/$1" "${HOME}/$1"' _ {} \;
     else
         # link file
         ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
