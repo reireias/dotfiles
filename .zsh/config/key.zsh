2ee63d2d53bb8b158157119f6fdce2f39d2262fe
reireias <reireias@gmail.com>
2019-12-16 12:29:20 +0900

    add key.zsh

---
+++ .zsh/config/key.zsh
@@ -0,0 +1,44 @@
+# key bind
+bindkey -e
+typeset -A key
+
+if [[ -n "${terminfo}" ]]; then
+    key[Home]="${terminfo[khome]}"
+    key[End]="${terminfo[kend]}"
+    key[Insert]="${terminfo[kich1]}"
+    key[Delete]="${terminfo[kdch1]}"
+    key[Up]="${terminfo[kcuu1]}"
+    key[Down]="${terminfo[kcud1]}"
+    key[Left]="${terminfo[kcub1]}"
+    key[Right]="${terminfo[kcuf1]}"
+    key[PageUp]="${terminfo[kpp]}"
+    key[PageDown]="${terminfo[knp]}"
+
+    # setup key accordingly
+    [[ -n "${key[Home]}"     ]]  && bindkey  "${key[Home]}"     beginning-of-line
+    [[ -n "${key[End]}"      ]]  && bindkey  "${key[End]}"      end-of-line
+    [[ -n "${key[Insert]}"   ]]  && bindkey  "${key[Insert]}"   overwrite-mode
+    [[ -n "${key[Delete]}"   ]]  && bindkey  "${key[Delete]}"   delete-char
+    [[ -n "${key[Up]}"       ]]  && bindkey  "${key[Up]}"       up-line-or-history
+    [[ -n "${key[Down]}"     ]]  && bindkey  "${key[Down]}"     down-line-or-history
+    [[ -n "${key[Left]}"     ]]  && bindkey  "${key[Left]}"     backward-char
+    [[ -n "${key[Right]}"    ]]  && bindkey  "${key[Right]}"    forward-char
+    [[ -n "${key[PageUp]}"   ]]  && bindkey  "${key[PageUp]}"   beginning-of-buffer-or-history
+    [[ -n "${key[PageDown]}" ]]  && bindkey  "${key[PageDown]}" end-of-buffer-or-history
+fi
+
+# word
+bindkey "\e[1;5C" forward-word
+bindkey "\e[1;5D" backward-word
+WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
+
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+    function zle-line-init () {
+        printf '%s' "${terminfo[smkx]}"
+    }
+    function zle-line-finish () {
+        printf '%s' "${terminfo[rmkx]}"
+    }
+    zle -N zle-line-init
+    zle -N zle-line-finish
+fi
