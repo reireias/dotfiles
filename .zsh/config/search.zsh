aa425b4588e1ba6b08cc68be589e0ad9d03d7288
reireias <reireias@gmail.com>
2020-11-12 18:36:09 +0900

    peco -> fzf

--- .zsh/config/search.zsh
+++ .zsh/config/search.zsh
@@ -1,71 +1,47 @@
+export FZF_DEFAULT_OPTS='--no-height --reverse'
+
 # shellcheck disable=SC2034
+function is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
+
 # Ctrl + R = history search
-function peco-history-selection() {
-    BUFFER=$(history -n 1 | tac  | awk '!a[$0]++' | peco)
+function fzf-history-selection() {
+    BUFFER=$(history -n 1 | tac  | awk '!a[$0]++' | fzf --sort --preview=)
     CURSOR=$#BUFFER
     zle reset-prompt
 }
-zle -N peco-history-selection
-bindkey '^R' peco-history-selection
+zle -N fzf-history-selection
+bindkey '^R' fzf-history-selection
 
 
 # Ctrl + G = ghq list
-function peco-ghq-look () {
+function fzf-ghq-look () {
     local selected_dir
     # shellcheck disable=SC2153
-    selected_dir=$(ghq list | peco --prompt="cd-ghq >" --query "$LBUFFER")
+    selected_dir=$(ghq list | fzf --preview=)
     if [ -n "$selected_dir" ]; then
         BUFFER="cd $(ghq list --full-path | grep --color=never -E "/$selected_dir$")"
         zle accept-line
     fi
 }
-zle -N peco-ghq-look
-bindkey '^G' peco-ghq-look
+zle -N fzf-ghq-look
+bindkey '^G' fzf-ghq-look
 
 
-# Ctrl + E = cdr
-function peco-cdr () {
-    local selected_dir
-    selected_dir="$(cdr -l | sed 's/^[0-9]\+ \+//' | peco --prompt="cdr >" --query "$LBUFFER")"
-    if [ -n "$selected_dir" ]; then
-        BUFFER="cd ${selected_dir}"
-        zle accept-line
-    fi
-}
-zle -N peco-cdr
-# bindkey '^E' peco-cdr
-
-
-# Ctrl + F = find and open
-function peco-find () {
-    local base="."
-    if [[ -n "$LBUFFER" ]] && [[ ! "$LBUFFER" =~ " $" ]]; then
-        local last_path
-        # shellcheck disable=SC2001
-        last_path="$(echo "$LBUFFER" | sed -e 's/^.*\ //g')"
-        if [ -d "$last_path" ]; then
-            base="$last_path"
-        fi
-    fi
-    local filepath
-    filepath="$(find "$base" | grep -v '/\.' | peco --prompt 'PATH>')"
-    [[ -z "$filepath" ]] && return
-    if [ -n "$LBUFFER" ]; then
-        if [[ "$base" = "." ]]; then
-            BUFFER="$LBUFFER$filepath"
-        else
-            # shellcheck disable=SC2001
-            BUFFER="$(echo "$LBUFFER" | sed -e "s#${base}\$##g")$filepath"
-        fi
+# Ctrl + F = find and vim
+function fzf-find () {
+    local selected
+    if is_in_git_repo; then
+        selected=$(git ls-files | fzf)
     else
-        if [[ -d "$filepath" ]]; then
-            BUFFER="cd $filepath"
-        elif [[ -f "$filepath" ]]; then
-            BUFFER="vi $filepath"
-        fi
+        selected=$(ag -g "" | fzf)
+    fi
+    if [ -n "$selected" ]; then
+        BUFFER="vi ${selected}"
         zle accept-line
     fi
-    CURSOR=$#BUFFER
 }
-zle -N peco-find
-bindkey '^F' peco-find
+zle -N fzf-find
+bindkey '^F' fzf-find
