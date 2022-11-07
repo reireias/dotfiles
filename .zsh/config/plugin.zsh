7eac99575666b740eb319404a16269138b401e0d
reireias <reireias@gmail.com>
2022-11-07 18:48:09 +0900

    fix: lint error

--- .zsh/config/plugin.zsh
+++ .zsh/config/plugin.zsh
@@ -1,13 +1,17 @@
+# shellcheck disable=SC2034
 if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
     print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
     command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    # shellcheck disable=SC2015
     command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
         print -P "%F{33} %F{34}Installation successful.%f%b" || \
         print -P "%F{160} The clone has failed.%f%b"
 fi
 
+# shellcheck disable=SC1091
 source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
 autoload -Uz _zinit
+# shellcheck disable=SC2154
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
 # zsh-completions
